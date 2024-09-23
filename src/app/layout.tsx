bb3dfcba91f7b27daee422d39aad07d2fdd0b65c
Malted <malted@malted.dev>
2024-09-23 13:08:07 -0400

    Remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const blackPearl = localFont({
   src: "./fonts/blackpearl.ttf",
   variable: "--font-blackpearl",
@@ -30,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${blackPearl.variable} antialiased`}>{children}</body>
+      <body className={`${blackPearl.variable} antialiased`}>{children}</body>
     </html>
   );
 }
