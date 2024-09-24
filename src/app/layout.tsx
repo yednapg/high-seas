3750aaf41efe7335dbe3caa9985ed8de4b133823
Malted <malted@malted.dev>
2024-09-24 13:43:34 -0400

    Add Vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import localFont from "next/font/local";
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={`${/*blackPearl.variable*/ 1} antialiased`}>
         {children}
       </body>
