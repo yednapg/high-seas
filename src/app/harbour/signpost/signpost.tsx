6cf88007b7f14a26263be7802f3d4d0206a37161
acon <101356372+acornitum@users.noreply.github.com>
2024-10-02 17:32:01 -0400

    signpost styling

--- src/app/harbour/signpost/signpost.tsx
+++ src/app/harbour/signpost/signpost.tsx
@@ -15,17 +15,20 @@ export default function SignPost({ wakaToken }: { wakaToken: string | null }) {
           : `WAZZUP! Your WakaTime token is ${wakaToken}`}
       </p>
 
-      <div className="m-4 max-w-xl text-white">
-        <p className="text-2xl mb-8">Key locations:</p>
-        <p className="mb-2 bg-blue-400 p-2 rounded-lg">{"The Keep: submit projects here when they're done!"}</p>
-        <p className="mb-2 bg-blue-400 p-2 rounded-lg">
+      <div className="m-4 text-white">
+        <p className="text-2xl mb-8 text-blue-500">Key locations:</p>
+        <div>
+                  <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">{"The Keep: submit projects here when they're done!"}</p>
+        <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
           Thunderdome: vote between projects others have made! After submitting
           your project, you must vote here in order to earn Scales.
         </p>
-        <p className="mb-2 bg-blue-400 p-2 rounded-lg">
+        <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
           Shoppe: spend all your scales here! Get items, ranging from Blahajs to
           Yubikeys.
         </p>
+        </div>
+
       </div>
     </motion.div>
   );
