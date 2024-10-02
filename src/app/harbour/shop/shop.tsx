b17d39fe2c08afeec0b5307c56b0407ce72b8178
Max Wofford <max@maxwofford.com>
2024-10-02 00:13:29 -0400

    Add some shop flavortext
    Did you know minecraft has over 400 splash text lines on the main menu?

--- src/app/harbour/shop/shop.tsx
+++ src/app/harbour/shop/shop.tsx
@@ -9,6 +9,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { SoundButton } from "./sound-button.js";
 import { LoadingSpinner } from "@/components/ui/loading_spinner.js";
+import { sample, shopBanner } from "../../../../lib/flavor.js";
 
 export default function Shop({ items }: any) {
   if (!items) {
@@ -30,7 +31,13 @@ export default function Shop({ items }: any) {
       className="container mx-auto px-4 py-8"
     >
       <SoundButton />
-      <h1 className="text-3xl font-bold mb-6">Ye olde shoppe</h1>
+
+      <div className="text-center">
+      <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl font-bold text-indigo-600 dark:text-indigo-300 mb-4">
+        Ye olde shoppe
+      </h1>
+      <p className="text-xl animate-pulse mb-6 rotate-[-7deg]">{sample(shopBanner)}</p>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {items.map((item: any) => (
           <motion.div key={item.id} whileHover={{ scale: 1.05 }}>
