b6f4b20b4b56d217cb2bad56155408151ce37292
Max Wofford <max@maxwofford.com>
2024-10-03 17:38:45 -0400

    Prevent prefetching login

--- src/components/sign_out.tsx
+++ src/components/sign_out.tsx
@@ -3,7 +3,7 @@ import { buttonVariants } from "./ui/button";
 
 export default function SignOut() {
   return (
-    <Link className={buttonVariants({ variant: "outline" })} href="/signout">
+    <Link className={buttonVariants({ variant: "outline" })} href="/signout" prefetch={false}>
       Sign out of Hack Club Slack
     </Link>
   );
