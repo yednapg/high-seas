d8855b8b03810976b4e095aff43c1bcc6a653ff1
acon <101356372+acornitum@users.noreply.github.com>
2024-10-03 10:45:45 -0400

    update signpost with example projects

--- src/app/harbour/signpost/signpost.tsx
+++ src/app/harbour/signpost/signpost.tsx
@@ -15,21 +15,43 @@ export default function SignPost({ wakaToken }: { wakaToken: string | null }) {
           : `WAZZUP! Your WakaTime token is ${wakaToken}`}
       </p>
 
-      <div className="m-4 text-white">
+      <div className="m-4">
         <p className="text-2xl mb-8 text-blue-500">Key locations:</p>
-        <div>
-                  <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">{"The Keep: submit projects here when they're done!"}</p>
-        <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-          Thunderdome: vote between projects others have made! After submitting
-          your project, you must vote here in order to earn Scales.
-        </p>
-        <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-          Shoppe: spend all your scales here! Get items, ranging from Blahajs to
-          Yubikeys.
-        </p>
+        <div className="text-white">
+          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">{"The Keep: submit projects here when they're done!"}</p>
+          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
+            Thunderdome: vote between projects others have made! After submitting
+            your project, you must vote here in order to earn Scales.
+          </p>
+          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
+            Shoppe: spend all your scales here! Get items, ranging from Blahajs to
+            Yubikeys.
+          </p>
         </div>
-
       </div>
+
+      <div className="m-4">
+        <p className="text-2xl mb-8 text-blue-500">Here are some example projects others have submitted!</p>
+        <div className="text-white">
+
+          <div className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
+            <p className="text-xl font-bold">Flip Slash Sprig Game</p>
+            <a href="https://github.com/kaj07/FLIP-SLASH--sprig-" target="_blank" rel="noopenner noreferrer" className="text-md text-pink-200">Project GitHub</a>
+            <p className="text-md">Hours spent: 8</p>
+            <p className="text-md">Scales earned: 4217</p>
+          </div>
+
+          <div className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
+            <p className="text-xl font-bold">FRC Robotics Simulation Game</p>
+            <a href="https://github.com/Brainiac11/sim_game" target="_blank" rel="noopenner noreferrer" className="text-md text-pink-200">Project GitHub</a>
+            <p className="text-md">Hours spent: 138</p>
+            <p className="text-md">Scales earned: 207493</p>
+          </div>
+
+        </div>
+      </div>
+
+
     </motion.div>
   );
 }
