a8fa6458c4fdd5794a0cfedf11da5f76c13df28d
acon <101356372+acornitum@users.noreply.github.com>
2024-10-03 15:17:52 -0400

    make scales symbol look prettier (bigger)

--- src/app/harbour/page.tsx
+++ src/app/harbour/page.tsx
@@ -107,7 +107,7 @@ export default function Harbour({ session }: { session: JwtPayload }) {
                 ),
               )}
               <div className="right-px absolute mr-2 text-green-400">
-                <div className="flex flex-row"><Image src={scales} alt="scales" width={10} height={10} />{personTicketBalance} Scales</div></div>
+                <div className="flex flex-row"><Image src={scales} alt="scales" width={25} height={25} /><span className="mr-2">{personTicketBalance} Scales</span></div></div>
             </TabsList>
             <div
               className="flex-1 overflow-auto p-3"
