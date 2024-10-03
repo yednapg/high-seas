fcab2f45e1d4531c56ad43ab0b2b9693a1a5d4cb
acon <101356372+acornitum@users.noreply.github.com>
2024-10-03 11:43:00 -0400

    styling on signpost section

--- src/app/harbour/signpost/signpost.tsx
+++ src/app/harbour/signpost/signpost.tsx
@@ -15,28 +15,33 @@ export default function SignPost({ wakaToken }: { wakaToken: string | null }) {
           : `WAZZUP! Your WakaTime token is ${wakaToken}`}
       </p>
 
-      <div className="m-4 flex flex-col justify-center items-center">
-        <p className="text-2xl mb-8 text-blue-500">Key locations:</p>
+      <div className="m-4 flex flex-col justify-center items-center mb-8">
+        <p className="text-2xl mb-6 text-blue-500">Key locations:</p>
         <div className="text-white">
-          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">{"The Keep: submit projects here when they're done!"}</p>
-          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-            Thunderdome: vote between projects others have made! After submitting
-            your project, you must vote between project matchups in order to earn Scales. Note that the number of Scales you earn is based on the number of hours you've put in your own project, and how it competes against other projects. Everything is peer voted!
-          </p>
-          <p className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-            Shoppe: spend all your scales here! Get items, ranging from Blahajs to
-            Yubikeys. More items to come in the future!
-          </p>
+          <div className="mb-2 bg-blue-400 p-4 rounded-lg max-w-xl">
+            <p className="text-2xl mb-2">The Keep</p>
+            <p>Submit your projects here when they're done!</p>
+          </div>
+          <div className="mb-2 bg-blue-400 p-4 rounded-lg max-w-xl">
+            <p className="text-2xl mb-2">Thunderdome</p>
+            <p>Vote between projects others have made! After submitting
+            your project, you must vote between project matchups in order to earn Scales. Note that the number of Scales you earn is based on the number of hours you've put in your own project, and how it competes against other projects. Everything is peer voted!</p>
+          </div>
+          <div className="mb-2 bg-blue-400 p-4 rounded-lg max-w-xl">
+            <p className="text-2xl mb-2">Shoppe</p>
+            <p>Spend all your scales here! Get items, ranging from Blahajs to
+            Yubikeys. More items to come in the future!</p>
+          </div>
         </div>
       </div>
 
       <div className="m-4 flex flex-col justify-center items-center">
-        <p className="text-2xl mb-8 text-blue-500">Here are some example projects others have submitted!</p>
+        <p className="text-2xl mb-6 text-blue-500">Here are some example projects others have submitted!</p>
         <div className="text-white">
 
-          <div className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-            <p className="text-xl font-bold">Flip Slash Sprig Game</p>
-            <div className="flex flex-row gap-8">
+          <div className="mb-2 bg-blue-400 p-4 px-8 rounded-lg max-w-[36rem]">
+            <p className="text-xl font-bold mb-2">Flip Slash Sprig Game</p>
+            <div className="flex flex-row justify-center items-center text-center gap-8">
               <a href="https://github.com/kaj07/FLIP-SLASH--sprig-" target="_blank" rel="noopenner noreferrer" className="text-md text-pink-200">Project GitHub</a>
               <p className="text-md">Hours spent: 8</p>
               <p className="text-md">Scales earned: 4217</p>
@@ -44,9 +49,9 @@ export default function SignPost({ wakaToken }: { wakaToken: string | null }) {
 
           </div>
 
-          <div className="mb-2 bg-blue-400 p-2 rounded-lg max-w-xl">
-            <p className="text-xl font-bold">FRC Robotics Simulation Game</p>
-            <div className="flex flex-row gap-8">
+          <div className="mb-2 bg-blue-400 p-4 px-8 rounded-lg max-w-[36rem]">
+            <p className="text-xl font-bold mb-2">FRC Robotics Simulation Game</p>
+            <div className="flex flex-row justify-center items-center text-center gap-8">
               <a href="https://github.com/Brainiac11/sim_game" target="_blank" rel="noopenner noreferrer" className="text-md text-pink-200">Project GitHub</a>
               <p className="text-md">Hours spent: 138</p>
               <p className="text-md">Scales earned: 207493</p>
