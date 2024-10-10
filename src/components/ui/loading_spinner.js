24c13324298f142cc90446913aeda8d925da4f30
Max Wofford <max@maxwofford.com>
2024-10-10 00:39:50 -0400

    Prevent loading spinner from showing unloaded image

--- src/components/ui/loading_spinner.js
+++ src/components/ui/loading_spinner.js
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react"
+import { useMemo, useEffect, useState } from "react"
 import { loadingSpinners, sample } from "../../../lib/flavor"
 
 const LoadingSpinner = () => {
   const [src, setSrc] = useState('')
+
   useEffect(() => {
     setSrc(sample(loadingSpinners))
   }, [])
 
-  return (
+  return useMemo(() => (
     <div className="h-16 w-16">
-      <img src={src} alt="loading spinner" />
+      {src && (
+        <img src={src} alt="loading spinner" />
+      )}
     </div>
-  )
+  ), [src])
 }
+
 export { LoadingSpinner }
