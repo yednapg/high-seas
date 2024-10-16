5022ce2c29f0d8846a6241e93bcb1ea1442f02ca
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-10-16 13:32:27 +0530

    make the tabs responsive on mobile

--- src/app/harbour/tabs/tabs.tsx
+++ src/app/harbour/tabs/tabs.tsx
@@ -12,7 +12,8 @@ import { getWaka } from "../../utils/waka";
 import { hasRecvFirstHeartbeat, getWakaEmail } from "../../utils/waka";
 import { getPersonTicketBalance } from "../../utils/airtable";
 import { WakaLock } from "../../../components/ui/waka-lock.js";
-
+import Pill from "@/components/ui/pill";
+import Image from "next/image";
 import useLocalStorageState from "../../../../lib/useLocalStorageState";
 import { useRouter } from "next/navigation";
 import { LoadingSpinner } from "@/components/ui/loading_spinner";
@@ -97,29 +98,38 @@ export default function Harbour({
       className="flex-1 flex flex-col"
       onValueChange={handleTabChange}
     >
-      <TabsList className="mx-2 my-2 relative">
-        {tabs.map((tab) =>
-          tab.name === "📮" ? (
-            <TabsTrigger
-              className="left-px absolute"
-              key={tab.name}
-              value={tab.path}
-            >
-              <img src="/signpost.png" width={20} alt="" />
-            </TabsTrigger>
-          ) : (
-            <TabsTrigger key={tab.name} value={tab.path}>
-              {tab.name}
-            </TabsTrigger>
-          ),
-        )}
-        <div className="right-px absolute mr-2 text-green-400">
+      <TabsList className="mx-2 my-2 relative flex items-center">
+        {tabs.map((tab) => (
+          <TabsTrigger
+            key={tab.name}
+            value={tab.path}
+            className={tab.name === "📮" ? "sm:absolute sm:left-px" : ""}
+          >
+            {tab.name === "📮" ? (
+              <>
+                <img src="/signpost.png" width={20} alt="Signpost" className="sm:block hidden" />
+                <span className="sm:hidden absolute"><Image src="/signpost.png" width={20} height={20} alt="Signpost"/></span>
+              </>
+            ) : (
+              tab.name
+            )}
+          </TabsTrigger>
+        ))}
+        <div className="right-px absolute mr-2 text-green-400 hidden sm:block">
           <div className="flex flex-row">
             <img src="scales.svg" alt="scales" width={25} height={25} />
             <span className="mr-2">{personTicketBalance} Scales</span>
           </div>
         </div>
       </TabsList>
+      {/* Scales Pill for mobile */}
+      <div className="flex flex-row justify-center text-green-400 sm:hidden">
+        <Pill
+          msg={`${personTicketBalance} Scales`}
+          color="green"
+          glyphImage={<img src="scales.svg" alt="scales" height={20} width={20} />}
+        />
+      </div>
       <div className="flex-1 overflow-auto p-3" id="harbour-tab-scroll-element">
         {tabs.map((tab) => (
           <TabsContent key={tab.name} value={tab.path} className="h-full">
