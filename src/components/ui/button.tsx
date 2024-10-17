ad142cc289f6320edc154fa5f254d58a2a2fced2
Malted <malted@malted.dev>
2024-10-17 12:36:54 -0400

    f

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-[#000000a0] text-secondary-foreground hover:bg-primary/90",
+        default: "bg-[#00000080] text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "bg-[#ffffffaa] text-secondary-foreground border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -45,7 +45,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         style={{
-          backgroundColor: "#ffffffe8",
           backgroundImage: "url(/textures/paper.png)",
           backgroundSize: "auto 500%",
           backgroundRepeat: "repeat-x",
