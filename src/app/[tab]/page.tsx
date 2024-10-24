61783f1f7b48772c9622e105d805af52734cfce4
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-24 15:05:52 -0400

    feat: add closure announcement for Low Skies

--- src/app/[tab]/page.tsx
+++ src/app/[tab]/page.tsx
@@ -25,6 +25,10 @@ export default async function Page({ params }: { params: { tab: string } }) {
       <div className="w-full min-h-screen pt-14 flex items-start justify-center p-8">
         <SoundButton />
         <Card className="w-full max-w-4xl flex flex-col" type={"cardboard"}>
+          <p className="text-center text-xl p-2">
+            Low Skies will be closed Saturday at Midnight EST. Start spending
+            those scales!!!
+          </p>
           <Harbour session={session} currentTab={tab} />
         </Card>
       </div>
