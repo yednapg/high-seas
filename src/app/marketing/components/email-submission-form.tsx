5d590dac3b947a9818aa167fc15f75e3a9bf8e12
acon <101356372+acornitum@users.noreply.github.com>
2024-10-25 19:39:58 -0400

    Fix margins on email text box for marketing page

--- src/app/marketing/components/email-submission-form.tsx
+++ src/app/marketing/components/email-submission-form.tsx
@@ -90,7 +90,7 @@ export default function EmailSubmissionForm() {
       <form
         ref={formRef}
         action={handleForm}
-        className="flex flex-wrap text-xl md:text-xl mt-6 justify-center items-center mx-4 rounded-xl border-[#3852CD] border-4 bg-[#3852CD]"
+        className="flex flex-wrap text-xl md:text-xl justify-center items-center mx-4 rounded-xl border-[#3852CD] border-4 bg-[#3852CD]"
       >
         <input
           type="text"
