323480fbc96df164ca0b721f256ed625bd9f6b39
Max Wofford <max@maxwofford.com>
2024-10-26 10:58:34 +0200

    Pirate shop title

--- src/app/harbour/shop/shop.tsx
+++ src/app/harbour/shop/shop.tsx
@@ -131,7 +131,7 @@ export default function Shop({ session }: { session: HsSession }) {
     <motion.div className="container mx-auto px-4 py-8">
       <div className="text-center">
         <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl font-bold text-indigo-600 dark:text-indigo-300 mb-4">
-          Ye olde shoppe
+          Pirate shop
         </h1>
         <p className="text-xl animate-pulse mb-6 rotate-[-7deg] inline-block">
           {bannerText}
