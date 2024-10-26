0844817b52c118e0f6a8fcb2ae075699c428184d
Aram <me@aram.sh>
2024-10-25 23:14:25 -0700

    change wording

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -92,7 +92,7 @@ export default async function Marketing() {
                 >
                   GitHub
                 </Link>
-                . For teens. Starts October 30. Ends January 31.
+                . For any teenager under 18. Starts October 30. Ends January 31.
               </p>
             </div>
           </div>
