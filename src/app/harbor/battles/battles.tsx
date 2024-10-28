e5f8d417202d04f58a773ab70125738d4a6f54e5
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2024-10-28 09:56:45 -0600

    Change "Project Matchup" to "Battle!"

--- src/app/harbor/battles/battles.tsx
+++ src/app/harbor/battles/battles.tsx
@@ -371,7 +371,7 @@ export default function Matchups({ session }: { session: HsSession }) {
       <div className="max-w-6xl mx-auto">
         <header className="text-center text-white mb-12">
           <h1 className="font-heading text-5xl mb-6 text-center relative w-fit mx-auto">
-            Project Matchup
+            Battle!
           </h1>
           <p className="text-xl text-gray-300 dark:text-gray-300 mb-4 max-w-3xl mx-auto">
             A good ship is technical, creative, and presented well so that
