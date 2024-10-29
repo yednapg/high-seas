6ce62a2bee8f07464994696b48c6dde823ddf122
Aram <me@aram.sh>
2024-10-28 21:08:24 -0700

    Showcase some magical hack clubber projects.

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -16,7 +16,7 @@ import divider from "./art/divider.png";
 import divider2 from "./art/divider2.png";
 // import paper from "./art/paper.png";
 
-import {IdeaGenerator} from "../../components/idea-generator.js"
+import { IdeaGenerator } from "../../components/idea-generator.js";
 
 import shop1 from "./art/shop/shop1.png";
 import shop2 from "./art/shop/shop2.png";
@@ -50,7 +50,7 @@ export default async function Marketing() {
             </div>
           </div>
         </div> */}
-        
+
         <div className="relative h-screen flex items-center justify-center">
           <div className="flex flex-col justify-center items-center text-center">
             <div className=" flex flex-col justify-center items-center mb-20">
@@ -64,18 +64,30 @@ export default async function Marketing() {
               </p>
               <div className="flex flex-wrap text-xl md:text-xl mt-6 justify-center items-center mx-4 rounded-xl ">
                 {session ? (
-                    <Link href="/signpost" className="p-4 rounded-lg text-md text-white bg-[#3852CD]">
-                      <img src="/signpost.png" width={32} className="inline-block mr-4" />
-                      Enter the harbor
-                    </Link>
+                  <Link
+                    href="/signpost"
+                    className="p-4 rounded-lg text-md text-white bg-[#3852CD]"
+                  >
+                    <img
+                      src="/signpost.png"
+                      width={32}
+                      className="inline-block mr-4"
+                    />
+                    Enter the harbor
+                  </Link>
                 ) : (
                   <EmailSubmissionForm />
                 )}
               </div>
-              <p className="mt-2 text-white bg-[#3852CD] bg-opacity-50 px-4 py-2 rounded-lg">New sailors get free stickers!</p>
+              <p className="mt-2 text-white bg-[#3852CD] bg-opacity-50 px-4 py-2 rounded-lg">
+                New sailors get free stickers!
+              </p>
             </div>
             <Link href="#howthisworks">
-              <img src="/arrows.svg" className="w-16 h-14 mb-6 opacity-60 bobble" />
+              <img
+                src="/arrows.svg"
+                className="w-16 h-14 mb-6 opacity-60 bobble"
+              />
             </Link>
 
             <div className="text-[#214495]">
@@ -102,7 +114,8 @@ export default async function Marketing() {
                 >
                   GitHub
                 </Link>
-                . For teenagers 18 and under. Starts October 30. Ends January 31.
+                . For teenagers 18 and under. Starts October 30. Ends January
+                31.
               </p>
             </div>
           </div>
@@ -485,9 +498,8 @@ export default async function Marketing() {
           </div>
 
           <div className="bg-black bg-opacity-50 py-12 my-12">
-
-          <div className="relative my-12">
-            {/*<div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+            <div className="relative my-12">
+              {/*<div className="absolute inset-0 bg-black opacity-50 z-10"></div>
             <div className="images-ani relative z-0">
               <div className="images-slide">
                 <img src="/pictures/pic1.png" alt="picture" className="" />
@@ -503,28 +515,31 @@ export default async function Marketing() {
               </div>
             </div>*/}
 
-
-            <div className="flex flex-col justify-center text-center">
-              <p className="text-5xl mt-12 mx-5 text-white">Join a community of makers</p>
+              <div className="flex flex-col justify-center text-center">
+                <p className="text-5xl mt-12 mx-5 text-white">
+                  Join a community of makers
+                </p>
+              </div>
             </div>
 
-            
-          </div>
-
-
-
-
             <div className="">
-
               <div className="flex flex-wrap justify-center items-center gap-10">
                 <div>
-                  <img src="/pictures/pic7.png" alt="hack clubbers!" className="md:max-w-[500px] border-dashed border-4 p-4 border-blue-800"/>
-                  <p className="flex flex-col justify-center items-center text-center">Hack Clubbers at the Boreal Express Hackathon!</p>
+                  <img
+                    src="/pictures/pic7.png"
+                    alt="hack clubbers!"
+                    className="md:max-w-[500px] border-dashed border-4 p-4 border-blue-800"
+                  />
+                  <p className="flex flex-col justify-center items-center text-center">
+                    Hack Clubbers at the Boreal Express Hackathon!
+                  </p>
                 </div>
-                
-                <iframe width="640" height="360"
-                  src="https://www.youtube.com/embed/hiG3fYq3xUU">
-                </iframe>
+
+                <iframe
+                  width="640"
+                  height="360"
+                  src="https://www.youtube.com/embed/hiG3fYq3xUU"
+                ></iframe>
               </div>
 
               <div className="flex flex-wrap justify-center items-center mb-12 gap-6 mx-5 my-12">
@@ -537,45 +552,30 @@ export default async function Marketing() {
                   <div className="relative my-4">
                     <div className="flex flex-row mb-2">
                       <img
-                        src="https://scrapbook.hackclub.com/_next/image?url=https%3A%2F%2Favatars.slack-edge.com%2F2024-05-06%2F7077145829972_8597fe575e09a698859c_192.png&w=48&q=75"
+                        src="https://ca.slack-edge.com/T0266FRGM-U06EMBJH71S-bea01757cf26-512"
                         className="w-10 h-10 rounded-full mr-2"
                       />
                       <p className="text-lg mb-2 flex flex-col justify-center">
-                        @elijah
+                        @lou
                       </p>
                     </div>
-                    <p className="text-sm mb-4">
-                      Finally shipped my personal ai clone and had a ton of fun
-                      playing around with it and seeing what other people did
-                      with it! Personal favorite was when it threatened to kill
-                      me and got very unhinged when the person threatened to
-                      send screenshots to me
-                    </p>
-                    <img src="https://scrapbook-into-the-redwoods.s3.amazonaws.com/4d4ecc40-c388-4b9d-997f-1f3d6a21302c-image.png" />
-                  </div>
-                </div>
-
-                <div className="relative overflow-hidden rounded-md p-6 max-w-[22rem]">
-                  <img
-                    src="howtobacks.svg"
-                    alt="image backing"
-                    className="absolute inset-0 w-full h-full object-cover opacity-100"
-                  />
-                  <div className="relative my-4">
-                    <div className="flex flex-row mb-2">
-                      <img
-                        src="https://scrapbook.hackclub.com/_next/image?url=https%3A%2F%2Favatars.slack-edge.com%2F2023-04-15%2F5116546887938_afb907f96fa13e434a49_192.png&w=48&q=75"
-                        className="w-10 h-10 rounded-full mr-2"
-                      />
-                      <p className="text-lg mb-2 flex flex-col justify-center">
-                        @cupcakes
-                      </p>
+                    <div>
+                      <p className="text-sm ">today i worked on:</p>
+                      <ul className="list-disc ml-5 text-sm mb-4">
+                        <li>an assignment on DDE for my robotics class</li>
+                        <li>
+                          started working on my chip-8 emulator but i switched
+                          tutorials since i wasn't rlly sure where to start. i
+                          think im getting the idea so i might go back to the
+                          one i was using to see if it makes more sense :)
+                        </li>
+                        <li>
+                          contacting a boba shop for discounted boba for
+                          counterspell since we're getting like 100
+                        </li>
+                      </ul>
                     </div>
-                    <p className="text-sm mb-4">Assembling blot robot! 🪛 </p>
-                    <img
-                      src="https://scrapbook-into-the-redwoods.s3.amazonaws.com/e75cf24a-46d9-45fa-92d3-b9e5862d0d47-img_2442.jpg"
-                      className="w-56"
-                    />
+                    <img src="https://cloud-3z0ux2fdz-hack-club-bot.vercel.app/0image.png" />
                   </div>
                 </div>
 
@@ -588,21 +588,62 @@ export default async function Marketing() {
                   <div className="relative my-4">
                     <div className="flex flex-row mb-2">
                       <img
-                        src="https://scrapbook.hackclub.com/_next/image?url=https://secure.gravatar.com/avatar/c2e358d7bf4677cac086556035ce1dbc.jpg?s%3D192%26d%3Dhttps%253A%252F%252Fa.slack-edge.com%252Fdf10d%252Fimg%252Favatars%252Fava_0011-192.png&w=640&q=75"
+                        src="https://ca.slack-edge.com/T0266FRGM-U03PSH1MRAA-74b6914da04c-512"
                         className="w-10 h-10 rounded-full mr-2"
                       />
                       <p className="text-lg mb-2 flex flex-col justify-center">
-                        @KonstantinosFragkoulis
+                        @whackalenso
                       </p>
                     </div>
                     <p className="text-sm mb-4">
-                      Well, the drone now should be able to follow the biggest
-                      object that it sees with a specific color. I {"haven't"}{" "}
-                      tested it yet though 😞 ({"I'm"} too scared to crash it).
-                      Here is a clip from earlier today, my genuine reaction to
-                      the first takeoff ever (got a bit scared at the end) 👍{" "}
+                      I finished my saxophone MIDI controller 🎷! It lets you
+                      play any software instrument you’d like using saxophone
+                      fingerings, allowing you to practice without disturbing
+                      others, create music in any DAW, and perform with it.
+                      Check it out at{" "}
+                      <Link href="https://github.com/Whackalenso/CircuitSax">
+                        https://github.com/Whackalenso/CircuitSax
+                      </Link>
                     </p>
-                    <img src="https://cloud-fshng6w8x-hack-club-bot.vercel.app/0videoframe_809.png" />
+                    <img src="https://cloud-7kbp6qhfd-hack-club-bot.vercel.app/0image.png" />
+                  </div>
+                </div>
+                <div className="relative overflow-hidden rounded-md p-6 max-w-[22rem]">
+                  <img
+                    src="howtobacks.svg"
+                    alt="image backing"
+                    className="absolute inset-0 w-full h-full object-cover opacity-100"
+                  />
+                  <div className="relative my-4">
+                    <div className="flex flex-row mb-2">
+                      <img
+                        src="https://ca.slack-edge.com/T0266FRGM-U04G40QKAAD-c1f0fac3b324-512"
+                        className="w-10 h-10 rounded-full mr-2"
+                      />
+                      <p className="text-lg mb-2 flex flex-col justify-center">
+                        @samuel
+                      </p>
+                    </div>
+                    <p className="text-sm mb-4">
+                      Just fixed some final bugs to ship the mobile app for Back
+                      on Track America, a nonprofit organization dedicated to
+                      serving the homeless and those suffering from food
+                      insecurity by coordinating student volunteers in several
+                      communities. The app allows volunteers to sign up for
+                      their location, receive announcements from location
+                      leaders, and discuss upcoming events! ⚒️ It's built with
+                      React Native & NativeWind on the frontend, and using
+                      Pocketbase on the backend. It's also now being submitted
+                      for <Link href="https://cider.hackclub.com">#cider</Link>,
+                      so that it can get published on the app store! An android
+                      build on the play store is also coming soon. Check out the
+                      code at https://github.com/backontrackus/app and
+                      https://github.com/backontrackus/backend
+                    </p>
+                    {/* <img
+                      src="https://scrapbook-into-the-redwoods.s3.amazonaws.com/e75cf24a-46d9-45fa-92d3-b9e5862d0d47-img_2442.jpg"
+                      className="w-56"
+                    /> */}
                   </div>
                 </div>
               </div>
@@ -729,7 +770,9 @@ export default async function Marketing() {
             <div className="flex flex-col">
               <p className="mb-2 text-pink-800 text-xl">Resources</p>
               <div className="text-pink-700">
-                <Link href="https://events.hackclub.com/">Community Events</Link>
+                <Link href="https://events.hackclub.com/">
+                  Community Events
+                </Link>
                 <br />
                 <Link href="https://jams.hackclub.com/">Jams</Link>
                 <br />
@@ -737,9 +780,13 @@ export default async function Marketing() {
                 <br />
                 <Link href="https://toolbox.hackclub.com/">Toolbox</Link>
                 <br />
-                <Link href="https://directory.hackclub.com/">Clubs Directory</Link>
+                <Link href="https://directory.hackclub.com/">
+                  Clubs Directory
+                </Link>
                 <br />
-                <Link href="https://hackclub.com/conduct/">Code of Conduct</Link>
+                <Link href="https://hackclub.com/conduct/">
+                  Code of Conduct
+                </Link>
                 <br />
               </div>
             </div>
