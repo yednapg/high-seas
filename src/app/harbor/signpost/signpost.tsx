7f3f4da4c70b2eb3625c9b9820e03f591418bb41
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-11-01 03:18:12 +0530

    Add hours logged in to The Sign Post

--- src/app/harbor/signpost/signpost.tsx
+++ src/app/harbor/signpost/signpost.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { getSelfPerson, getSignpostUpdates } from "../../utils/airtable";
 import Verification from "./verification";
@@ -9,9 +9,10 @@ import useLocalStorageState from "../../../../lib/useLocalStorageState";
 import Platforms from "@/app/utils/wakatime-setup/platforms";
 import JaggedCard from "../../../components/jagged-card";
 import { fetchWaka, SignpostFeedItem } from "@/app/utils/data";
-
+import { getWakaSessions } from "@/app/utils/waka";
 export default function SignPost({ session }: { session: any }) {
   const [wakaKey, setWakaKey] = useLocalStorageState("cache.wakaKey", "");
+  const [trackedHours, setTrackedHours] = useState<number>();
   const motionProps = useMemo(
     () => ({
       initial: { opacity: 0 },
@@ -22,7 +23,7 @@ export default function SignPost({ session }: { session: any }) {
 
   const [verification, setVerification] = useLocalStorageState(
     "cache.verification",
-    "Eligible L1", // load in verified by default to prevent warning sign "popping" in 
+    "Eligible L1",
   );
   const [reason, setReason] = useLocalStorageState("cache.reason", "");
   const [signpostUpdates, setSignpostUpdates] = useLocalStorageState<SignpostFeedItem[]>("cache.signpost", []);
@@ -30,6 +31,13 @@ export default function SignPost({ session }: { session: any }) {
 
   useEffect(() => {
     fetchWaka().then(({ key }) => setWakaKey(key));
+    const initHours = async () => {
+      const sessions = await getWakaSessions();
+      const totalSeconds = sessions.projects.reduce((acc, project) => acc + project.total, 0);
+      const totalHours = totalSeconds / 3600;
+      setTrackedHours(totalHours);
+    };
+    initHours();
 
     if ((new Date()).getTime() - lastSignpostUpdate > 1000 * 60 * 15) {
       getSignpostUpdates().then((data) => {
@@ -53,26 +61,35 @@ export default function SignPost({ session }: { session: any }) {
       {...motionProps}
       className="container mx-auto px-4 py-8 max-w-prose"
     >
-      <h1 className="font-heading text-5xl font-bold text-white mb-6 text-center">
+      <h1 className="font-heading text-5xl font-bold text-white mb-2 text-center">
         The Signpost
       </h1>
-        <Verification status={verification} reason={reason} />
+      
+      <div className="flex justify-center my-6">
+        <div className="flex items-center gap-1 text-white bg-opacity-20 bg-white px-4 py-2 rounded-lg border border-white/20 backdrop-blur-sm">
+          <span className="text-xs">⏱️</span>
+          <span className="font-medium">{trackedHours?.toFixed(1) || "-"}</span>
+          <span className="sm:inline">hours logged in Hackatime</span>
+        </div>
+      </div>
 
-        {signpostUpdates
-        ? signpostUpdates.map(
-          (update: any, index: number) =>
-            update?.["fields"]?.["visible"] && (
-            <JaggedCard
-              key={index}
-              className={`text-[${update?.["fields"]?.["text_color"]}}]`}
-              bgColor={update?.["fields"]?.["background_color"]}
-            >
-              <span className="text-bold text-xl">{update?.["fields"]?.["title"]}</span>
-              <p>{update?.["fields"]?.["content"]}</p>
-            </JaggedCard>
-            ),
-          )
-        : null}
+      <Verification status={verification} reason={reason} />
+
+      {signpostUpdates
+      ? signpostUpdates.map(
+        (update: any, index: number) =>
+          update?.["fields"]?.["visible"] && (
+          <JaggedCard
+            key={index}
+            className={`text-[${update?.["fields"]?.["text_color"]}}]`}
+            bgColor={update?.["fields"]?.["background_color"]}
+          >
+            <span className="text-bold text-xl">{update?.["fields"]?.["title"]}</span>
+            <p>{update?.["fields"]?.["content"]}</p>
+          </JaggedCard>
+          ),
+        )
+      : null}
 
       <JaggedCard className="text-white">
         {wakaKey ? (
