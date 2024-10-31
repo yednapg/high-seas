a64f754edf86287a33671818691b0944452891e2
Max Wofford <max@maxwofford.com>
2024-10-31 11:31:59 -0400

    Skip check for CRON_SECRET in dev

--- src/app/api/cron/every-minute/route.ts
+++ src/app/api/cron/every-minute/route.ts
@@ -2,12 +2,13 @@ import type { NextRequest } from 'next/server';
 import { processBackgroundJobs } from './process-background-jobs';
 
 export async function GET(request: NextRequest) {
-  // const authHeader = request.headers.get('authorization');
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response('Unauthorized', {
-  //     status: 401,
-  //   });
-  // }
+  const authHeader = request.headers.get('authorization');
+  const isDev = process.env.NODE_ENV === 'development';
+  if (!isDev && authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
 
   await processBackgroundJobs()
 
