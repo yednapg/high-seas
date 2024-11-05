35d6ff16c2b18f2a62b24af3234e23baedf04d9a
Max Wofford <max@maxwofford.com>
2024-11-05 14:50:50 -0500

    Remove 33d6a6 from showing on card updates

--- src/app/harbor/signpost/feed-items.tsx
+++ src/app/harbor/signpost/feed-items.tsx
@@ -42,7 +42,6 @@ export default function FeedItems() {
               <br />
               <span style={{ color: `#${item.textColor}` }}>
                 <Markdown>{item.content}</Markdown>
-                {item.backgroundColor}
               </span>
             </p>
           </JaggedCardSmall>
