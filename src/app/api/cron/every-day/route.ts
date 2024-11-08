455f65b317f394cd2050733267b1f1de21a0f9d7
Max Wofford <max@maxwofford.com>
2024-11-08 10:23:58 -0500

    Add public messages for the lottery

--- src/app/api/cron/every-day/route.ts
+++ src/app/api/cron/every-day/route.ts
@@ -5,9 +5,18 @@ Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
   endpointUrl: 'https://middleman.hackclub.com/airtable/v0',
 })
+
 const base = Airtable.base('appTeNFYcUiYfGcR6')
 
+const highSeasChannelId = 'C07PZMBUNDS'
+
 async function processDailyJobs() {
+  const startingMessage = base('arrpheus_message_requests').create({
+    message_text: `Each day, a newly signed up user will win a free Raspberry Pi Zero! Today's winner is...`,
+    target_slack_id: highSeasChannelId,
+    requester_identifier: 'cron-job',
+  })
+
   // read all free sticker orders created in the last 24 hours
   const eligibleUsers = await base('people')
     .select({
@@ -21,15 +30,26 @@ async function processDailyJobs() {
   const winner = new Array(eligibleUsers).sort(() => Math.random() - 0.5)[0]
 
   // create the order
-
-  const order = await base('orders').create({
+  const order = base('orders').create({
     shop_item: 'item_free_raspberry_pi_zero',
     recipient: [winner?.id],
   })
 
   // send a DM to the winner
+  const dmMessage = base('arrpheus_message_requests').create({
+    message_text: `Congratulations to <@${winner?.fields['slack_id']}> for winning a free Raspberry Pi Zero! 🎉`,
+    target_slack_id: winner?.fields['slack_id'],
+    requester_identifier: 'cron-job',
+  })
 
   // post in the #high-seas channel
+  const publicMessage = base('arrpheus_message_requests').create({
+    message_text: `Congratulations to <@${winner?.fields['slack_id']}> for winning a free Raspberry Pi Zero! 🎉`,
+    target_slack_id: highSeasChannelId,
+    requester_identifier: 'cron-job',
+  })
+
+  await Promise.all([order, dmMessage, publicMessage])
 }
 
 export async function GET(request: NextRequest) {
