ec1f98b40cea85413bbfc9dcd1195db6f70e4c0c
Max Wofford <max@maxwofford.com>
2024-11-19 17:39:04 -0500

    Switch to using autonumber

---
+++ src/app/api/referral/[autonum]/route.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import { getPersonByAuto } from '@/app/utils/airtable'
+import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
+
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: { autonum: string } },
+) {
+  const slackId = (await getPersonByAuto(params.autonum))?.slackId
+  if (slackId) {
+    redirect('/')
+  } else {
+    redirect('/?ref=' + slackId)
+  }
+}
