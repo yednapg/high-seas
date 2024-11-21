7a98eb48ab21b03466799f99ecb5eeb0bfb007eb
MoPaMo <mopamo@users.noreply.github.com>
2024-11-21 22:19:18 +0100

    fix: ensure progress bar width does not exceed maximum value and improve item ring color transition

--- src/app/harbor/shop/progress.tsx
+++ src/app/harbor/shop/progress.tsx
@@ -25,7 +25,12 @@ export default function Progress({ val, items }) {
         <div className="relative w-full h-2.5 bg-gray-300 rounded overflow-visible">
           <div
             className="w-0 h-full bg-blue-500 rounded transition-width duration-300"
-            style={{ width: (currentTix * 100) / max.priceGlobal + '%' }}
+            style={{
+              width:
+                (currentTix <= max.priceGlobal
+                  ? (currentTix * 100) / max.priceGlobal
+                  : '100') + '%',
+            }}
           ></div>
           {favItems.map((item) => (
             <div
@@ -35,9 +40,14 @@ export default function Progress({ val, items }) {
               <img
                 src={item.imageUrl}
                 alt={item.name}
-                className="rounded-full h-8 bg-white ring-2 ring-black aspect-square object-cover "
+                className={
+                  'rounded-full h-8 bg-white ring-2 aspect-square object-cover transition-left duration-300' +
+                  (currentTix >= item.priceGlobal
+                    ? ' ring-blue-500'
+                    : ' ring-black')
+                }
               />
-              <div className="bg-black m-1 rounded-sm flex w-full">
+              <div className="bg-black m-1 rounded-sm flex w-full justify-center">
                 {item.priceGlobal}
               </div>
             </div>
