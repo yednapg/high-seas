6d894e02103680c24fbf7c3a8a7e840cc25b0b3d
Max Wofford <max@maxwofford.com>
2024-11-28 11:46:26 -0500

    Add redis lock to new ships

--- src/app/harbor/shipyard/new-ship-form.tsx
+++ src/app/harbor/shipyard/new-ship-form.tsx
@@ -10,6 +10,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { useToast } from '@/hooks/use-toast'
 import Icon from '@hackclub/icons'
 import { MultiSelect } from '../../../components/ui/multi-select'
+import { withLock } from '../../../../lib/redis-lock'
 
 async function testReadmeLink(url: string) {
   const response = await fetch(url)
@@ -129,7 +130,9 @@ export default function NewShipForm({
 
     const isTutorial = sessionStorage?.getItem('tutorial') === 'true'
     if (!isTutorial) {
-      await createShip(formData)
+      await withLock(`ship:${session.user_id}`, () => (
+        createShip(formData, false)
+      ))
     }
     confettiRef.current?.addConfetti()
     closeForm()
