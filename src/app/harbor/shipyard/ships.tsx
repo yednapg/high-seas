d407f6af44238ca5d52a77e0a488102f28aa3999
Max Wofford <max@maxwofford.com>
2024-11-29 01:22:55 -0500

    Enable shipping

--- src/app/harbor/shipyard/ships.tsx
+++ src/app/harbor/shipyard/ships.tsx
@@ -225,11 +225,11 @@ export default function Ships({
                 <Button
                   onClick={async (e) => {
                     e.stopPropagation()
-                    alert(
-                      'Blimeys!! Sorry, but updates are broken right now – the dev team is working on a fix',
-                    )
-                    //console.log('Shipping an update...', s)
-                    //setNewUpdateShip(s)
+                    // alert(
+                    //   'Blimeys!! Sorry, but updates are broken right now – the dev team is working on a fix',
+                    // )
+                    console.log('Shipping an update...', s)
+                    setNewUpdateShip(s)
                   }}
                 >
                   Ship an update!
